a8bf0f22628977fd68cce55259c75536b3df6f41
Sehyeon Park <152643439+UBDBD@users.noreply.github.com>
2026-06-27 21:14:52 +0900

    Fix logic and variable names in 양과 늑대.py

--- Computer Programming/양과 늑대.py
+++ Computer Programming/양과 늑대.py
@@ -1,30 +1,30 @@
 # 2022 KAKAO BLIND RECRUITMENT > 양과 늑대
 
 def solution(info, edges):    
-    answer = 1
+    answer = 0
     
-    children = [[] for _ in range(len(info))]
+    child = [[] for _ in range(len(info))]
     
-    for parent, child in edges:
-        children[parent].append(child)
+    for p, c in edges:
+        child[p].append(c)
+        
+    s = [(1, 0, child[0])]
     
-    stack = [(1, 0, children[0])]
-    
-    while stack:
-        sheep, wolf, candidates = stack.pop()
+    while s:
+        sheep, wolf, candidates = s.pop()
         answer = max(answer, sheep)
         
-        for node in candidates:
-            next_sheep = sheep + (info[node]==0)
-            next_wolf = wolf + (info[node]==1)
+        for i in candidates:
+            nxt_sheep = sheep + (info[i]==0)
+            nxt_wolf = wolf + (info[i]==1)
             
-            if next_wolf >= next_sheep:
+            if nxt_wolf >= nxt_sheep:
                 continue
-                
-            next_candidates = candidates.copy()
-            next_candidates.remove(node)
-            next_candidates.extend(children[node])
             
-            stack.append((next_sheep, next_wolf, next_candidates))
-        
+            nxt_candidates = candidates.copy()
+            nxt_candidates.remove(i)
+            nxt_candidates.extend(child[i])
+            
+            s.append((nxt_sheep, nxt_wolf, nxt_candidates))
+                
     return answer
